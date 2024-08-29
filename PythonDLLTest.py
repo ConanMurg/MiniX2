a56c897dbf6e702a765199479e0935b7bdbbe844
Conan Murgatroyd <conanmurg@gmail.com>
2024-08-29 17:25:31 +0100

    Small Changes

--- PythonDLLTest.py
+++ PythonDLLTest.py
@@ -1,9 +1,24 @@
 import ctypes
-
+import time
 # Load the shared library
 mydll = ctypes.CDLL('./libgccDppConsoleMiniX2.so')
 result = mydll.ConnectToDefaultDPP()
 test = mydll.GetDppStatus()
+
+print('Interlock Status')
+test2 = mydll.GetInterlockStatus()
+
+
+test3 = mydll.ReadHVCfg()
+
+
+print('Turning On')
+on = mydll.TurnHVOff()
+
+test4 = mydll.ReadHVCfg()
+
+
+
 # mydll = ctypes.CDLL('./libmydll.so')
 
 
